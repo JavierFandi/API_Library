2f5239e1c78e1d2f75b152049f6871e259622e31
JavierFandi <javier.fandi@hotmail.com>
2024-01-23 21:29:49 -0300

    Delete

--- middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500).json({
-        message: err.message || "Error in request to " + req.url
-    });
-};
-
-module.exports = errorHandler;
